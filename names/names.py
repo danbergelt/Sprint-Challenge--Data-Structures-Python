48d93bffbb6778b2526f509422bb58a7037a3723
Dan <danbergelt@protonmail.com>
2019-12-20 12:38:28 -0500

    names complete

--- names/names.py
+++ names/names.py
@@ -1,5 +1,4 @@
 import time
-from bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -12,13 +11,20 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+<<<<<<< HEAD
 tree = BinarySearchTree(names_1[0])
 [tree.insert(name) for name in names_1[1:]]
 [duplicates.append(duplicate) for duplicate in names_2 if tree.contains(duplicate)]
+=======
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
+>>>>>>> parent of 602df17... names complete
 
 end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
